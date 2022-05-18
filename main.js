16059aa8975e0ef1663b32e4495c33315fe778fc
Iggor de Souza <iggordesouza@hotmail.com>
2022-05-17 22:42:15 -0400

    Criado slider de imagens

--- main.js
+++ main.js
@@ -71,3 +71,27 @@ ScrollReveal({
 #about,
 #about header,
 #about .content`)
+
+let time = 5000,
+  currentImageIndex = 0,
+  images = document.querySelectorAll('#slider img')
+max = images.length
+
+function nextImage() {
+  images[currentImageIndex].classList.remove('selected')
+
+  currentImageIndex++
+
+  if (currentImageIndex >= max) currentImageIndex = 0
+
+  images[currentImageIndex].classList.add('selected')
+}
+
+function start() {
+  setInterval(() => {
+    // troca de image
+    nextImage()
+  }, time)
+}
+
+window.addEventListener('load', start)
